ee5b80847a96493b6773f19b85382ccb147c9934
Pedro Silva <pedrohenrique2604@gmail.com>
2021-04-04 00:29:30 -0300

    Adição da biblioteca leaflet(mapa), criação do arquivo mapa.json, com dados mockados de postos de saude, adição das opções resolveJsonModule/allowSyntheticDefaultImports ao compilador do angular

--- src/app/mapa/mapa.page.ts
+++ src/app/mapa/mapa.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {Map,tileLayer,marker} from 'leaflet';
+import mapaMock from '../services/mocks/mapa.json';
+
+/*
+	Referências leaflet
+	https://leafletjs.com/reference-1.7.1.html
+	https://medium.com/@bviveksingh96/using-leaflet-with-ionic-4-f7acbd1c2464
+*/
 
 @Component({
   selector: 'app-mapa',
@@ -7,9 +15,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaPage implements OnInit {
 
+	public map:Map;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter() {
+    this.inicializarMapa();
+  }
+
+  inicializarMapa () {
+  	const attribution = 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
+  	const titleLayerUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  	const coordenadasIniciais = mapaMock.coordenadasIniciais;
+  	const postosSaude = mapaMock.postosSaude;
+
+  	this.map = new Map("mapid").setView([coordenadasIniciais.lat, coordenadasIniciais.lng], 13);
+
+  	tileLayer(titleLayerUrl, {attribution}).addTo(this.map);
+
+  	for (let posto of postosSaude) {
+	  	let postoMarker = marker(
+	  		[posto.coordenadas.lat, posto.coordenadas.lng]
+  		).addTo(this.map);
+
+  		postoMarker.bindTooltip(posto.nome);
+  	}
+  }
 }
