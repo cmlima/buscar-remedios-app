1008f44670de3f3dd1799a9eb513ec2380aa5abe
Caio Moysés de Lima <caio.moyses.lima@gmail.com>
2021-04-04 01:05:00 -0300

    Implementando tela de detalhes da receita

--- src/app/detalhes-receita/detalhes-receita.page.ts
+++ src/app/detalhes-receita/detalhes-receita.page.ts
@@ -2,22 +2,46 @@ import { Component, OnInit } from '@angular/core';
 import { ReceitasService } from '../services/receitas.service';
 import { Receita } from '../services/entities/receita';
 import { gerarInt } from '../services/mocks/random.js';
-
+import { MensagensService } from '../services/mensagens.service';
 @Component({
   selector: 'app-detalhes-receita',
   templateUrl: './detalhes-receita.page.html',
   styleUrls: ['./detalhes-receita.page.scss'],
 })
 export class DetalhesReceitaPage implements OnInit {
-  private receita: Receita;
+  public receita: Receita;
+  public dataURL: string;
 
-  constructor(private receitaService: ReceitasService) { }
+  constructor(private receitaService: ReceitasService, private mensagensService: MensagensService) { }
 
   ngOnInit() {
     const receitas = this.receitaService.getReceitas();
     console.log('Receitas: ', receitas);
     this.receita = receitas[gerarInt(0, receitas.length - 1)];
     console.log(this.receita);
+    this.receitaService.gerarQRCode(this.receita._id).then(dataURL => {
+      this.dataURL = dataURL;
+      console.log(this.dataURL);
+    }).catch(e => {
+      this.mensagensService.erro('Falha no QRCode', e.message);
+    });
+  }
+
+  public async baixarReceita() {
+    this.mensagensService.carregando('Carregando...', 5000, (aviso => {
+      setTimeout(() => {
+        console.log('baixado!');
+        aviso.dismiss();
+      }, 1000);
+    }));
+  }
+
+  public async removerReceita() {
+    this.mensagensService.confirmar('Confirmação', 'Tem certeza de que deseja remover a receita da memória do aparelho?', (yes: boolean) => {
+      if (yes) {
+        console.log('removendo...');
+      }
+    })
   }
 
 }
