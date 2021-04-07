ed6388db2b420c0dfd975de708eb616c6f29c90c
Caio Moysés de Lima <caio.moyses.lima@gmail.com>
2021-04-07 09:18:35 -0300

    Refatorando serviço de scanner

--- src/app/services/scanner.service.ts
+++ src/app/services/scanner.service.ts
@@ -1,25 +1,23 @@
 import { Injectable } from '@angular/core';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-
+import { MensagensService } from './mensagens.service';
 @Injectable({
   providedIn: 'root'
 })
 export class ScannerService {
 
-  constructor(private scanner: BarcodeScanner) { }
+  constructor(private scanner: BarcodeScanner, private mensagensService: MensagensService) { }
 
-   public scan(): Promise<string | boolean> {
-     return new Promise((resolve, reject) => {
-      this.scanner.scan().then(data => {
-        if (data.cancelled) resolve(false);
-        if (data.format !== 'QR_CODE') {
-          reject('Formato não reconhecido!');
-        }
-        resolve(data.text);
-      }).catch(e => {
-        console.error(e);
-        reject('Erro de leitura!');
-      });
-     })
+   public async scan(): Promise<string | false> {
+     try {
+      const response = await this.scanner.scan();
+      if (response.cancelled) return false;
+      if (response.format !== 'QR_CODE') {
+        throw new Error('Formato não permitido!')
+      }
+      return response.text;
+     } catch (e) {
+      this.mensagensService.erro('Falha de Leitura', e.message);
+     }
   }   
 }
