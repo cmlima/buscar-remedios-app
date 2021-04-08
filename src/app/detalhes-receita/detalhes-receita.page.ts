f99efd79cfc9cb00be7c4ba6c5ef75b7c82b053f
leticiasantoos <61153562+leticiasantoos@users.noreply.github.com>
2021-04-08 14:21:47 -0300

    Preparando a página detalhes-receitas para implementação da página lista-receitas

--- src/app/detalhes-receita/detalhes-receita.page.ts
+++ src/app/detalhes-receita/detalhes-receita.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ReceitasService } from '../services/receitas.service';
 import { Receita } from '../services/entities/receita';
-import { gerarInt } from '../services/mocks/random.js';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-detalhes-receita',
   templateUrl: './detalhes-receita.page.html',
@@ -13,12 +14,11 @@ export class DetalhesReceitaPage implements OnInit {
   // https://www.labnol.org/internet/direct-links-for-google-drive/28356/
   public downloadLink: string = 'https://drive.google.com/uc?export=download&id=1yfrXjlyzikuYsZai2sv-eoF0UuK9Srr-';
 
-  constructor(private receitaService: ReceitasService) { }
+  constructor(private receitaService: ReceitasService, private router: Router) { }
 
   async ngOnInit() {
-    const receitas = this.receitaService.getReceitas();
-    console.log('Receitas: ', receitas);
-    this.receita = receitas[gerarInt(0, receitas.length - 1)];
+    // TODO: Eliminar mocks
+    this.receita = this.receitaService.getSelecionada();
     console.log(this.receita);
     const response = await this.receitaService.gerarQRCode(this.receita._id);
     this.dataURL = response ? response : '';
@@ -26,6 +26,7 @@ export class DetalhesReceitaPage implements OnInit {
     
   public removerReceita() {
     this.receitaService.remover(this.receita._id);
+    this.router.navigate(['/lista-receitas']);
   }
 
 }
