15556988e0f234ae0bc5546370bd8fd2db50c5bb
Caio Moysés de Lima <caio.moyses.lima@gmail.com>
2021-04-08 21:49:21 -0300

    Dando forma final à página inicial, escondendo atalhos em uma action sheet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 import { ScannerService } from '../services/scanner.service';
 import { ReceitasService } from '../services/receitas.service';
 @Component({
@@ -8,11 +10,43 @@ import { ReceitasService } from '../services/receitas.service';
 })
 export class HomePage {
 
-  constructor(private scannerService: ScannerService, private receitasService: ReceitasService) { }
+  constructor(private scannerService: ScannerService, private receitasService: ReceitasService, private actionSheetController: ActionSheetController, private router: Router) { }
 
   public async lerQRCode() {
     const hash = await this.scannerService.scan();
     if (hash) this.receitasService.buscar(hash);
+  }
+
+  // TODO: remover em produção
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Acessar Diretamente:',
+      buttons: [
+        {
+          text: 'Letícia (Lista de Receitas)',
+          role: 'navigation',
+          icon: 'open',
+          handler: () => this.router.navigate(['/lista-receitas'])
+        }, {
+          text: 'Caio (Detalhes da Receita)',
+          role: 'navigation',
+          icon: 'open',
+          handler: () => this.router.navigate(['/detalhes-receita'])
+        }, {
+          text: 'Pedro (Posto de Saúde - Mapa)',
+          role: 'navigation',
+          icon: 'open',
+          handler: () => this.router.navigate(['/mapa'])
+        }, {
+          text: 'Álef (Posto de Saúde - Detalhes)',
+          role: 'navigation',
+          icon: 'open',
+          handler: () => this.router.navigate(['/lista-postos'])
+        }
+      ]
+    });
+  
+    await actionSheet.present();
   }
 
 }
