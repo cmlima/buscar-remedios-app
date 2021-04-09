6f15e5e64c50c2fbaeb63e7bc94d71c43680ca4c
Caio Moysés de Lima <caio.moyses.lima@gmail.com>
2021-04-08 21:48:18 -0300

    Aprimorando serviço de receitas para facilitar depois a implementação da persistência local

--- src/app/services/receitas.service.ts
+++ src/app/services/receitas.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Receita } from './entities';
-import { gerarInt, gerarReceitas } from './mocks/random.js';
+import { gerarReceita, gerarReceitas } from './mocks/random.js';
 import QRCode from 'qrcode';
 import { MensagensService } from './mensagens.service';
 
@@ -8,7 +8,8 @@ import { MensagensService } from './mensagens.service';
   providedIn: 'root'
 })
 export class ReceitasService {
-  private _receitas: Receita[] = [];
+  // TODO: excluir mock
+  private _receitas: Receita[] = gerarReceitas(10);
   private _selecionada: Receita;
   
   constructor(private mensagensService: MensagensService) { }
@@ -16,37 +17,53 @@ export class ReceitasService {
   public async getReceitas(): Promise<Receita[] | false> {
     // TODO: implementar leitura do storage local
     try {
-      this._receitas = gerarReceitas(10).sort((a, b) => {
-        return Number(b.data.split('/').reverse().join()) - Number(a.data.split('/').reverse().join());
-      })      
-      return await Promise.resolve(this._receitas);
+      return await Promise.resolve(this._receitas.sort((a, b) => {
+        const aValue = Number(a.data.split('/').reverse().join(''));
+        const bValue = Number(b.data.split('/').reverse().join(''));
+        return bValue - aValue;
+      }));
     } catch (e) {
       this.mensagensService.erro('Falha na recuperação das receitas', e.message);
       return false;
     }
   }
 
-  public getSelecionada(): Receita {
-    // TODO: Eliminar mock
-    return this._selecionada ? this._selecionada : this._receitas[gerarInt(0, this._receitas.length - 1)];
+  public getSelecionada(): Receita | false {
+    console.log(this._selecionada);
+    if (this._selecionada) return this._selecionada;
+    return false;
   }
 
   public selecionar(receita: Receita) {
     this._selecionada = receita;
   }
-  public async buscar(hash: string) {
+
+  public async buscar(hash: string): Promise<Receita | false> {
     // TODO: Implementar consumo API remota, 
     // atualizando vetor de receitas em caso de sucesso
     // ou retornando mensagem de erro em caso de insucesso
     console.log('Buscando... ', hash);
+    const receita = gerarReceita();
+    if (receita) {
+      this._receitas.push(receita);
+      return await Promise.resolve(receita);
+    }
+    await this.mensagensService.erro('', 'Receita não localizada!');
+    return Promise.resolve(false);
   }
 
-  public remover(hash: string) {
-    this.mensagensService.confirmar('Confirmação', 'Tem certeza de que deseja remover a receita da memória do aparelho?', (yes: boolean) => {
-      if (yes) {
-        // TODO: implementar remoção do storage local
-        console.log('removendo... ', hash);
-      }
+  public async remover(hash: string): Promise<boolean> {
+    return new Promise((resolve) => {
+      this.mensagensService.confirmar('Confirmação', 'Tem certeza de que deseja remover a receita da memória do aparelho?', (yes: boolean) => {
+        if (yes) {
+          // TODO: implementar remoção do storage local
+          console.log('removendo... ', hash);
+          const index = this._receitas.findIndex(receita => receita._id === hash);
+          console.log('data: ', this._receitas[index].data);
+          this._receitas.splice(index, 1);
+        }
+        resolve(yes);
+      });
     })
   }
 
@@ -54,7 +71,7 @@ export class ReceitasService {
     try {
       return await QRCode.toDataURL(hash);
     } catch (e) {
-      this.mensagensService.erro('Falha no QRCode', e.message);
+      await this.mensagensService.erro('Falha no QRCode', e.message);
       return false;
     }
   }
