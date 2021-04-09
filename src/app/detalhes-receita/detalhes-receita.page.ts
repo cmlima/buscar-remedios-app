87ea7e921879fa619743b375458161bc945dd57f
Caio Moysés de Lima <caio.moyses.lima@gmail.com>
2021-04-08 21:50:51 -0300

    Aprimorando página de detalhes da receita

--- src/app/detalhes-receita/detalhes-receita.page.ts
+++ src/app/detalhes-receita/detalhes-receita.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MensagensService } from '../services/mensagens.service';
 import { ReceitasService } from '../services/receitas.service';
 import { Receita } from '../services/entities/receita';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhes-receita',
@@ -14,19 +15,41 @@ export class DetalhesReceitaPage implements OnInit {
   // https://www.labnol.org/internet/direct-links-for-google-drive/28356/
   public downloadLink: string = 'https://drive.google.com/uc?export=download&id=1yfrXjlyzikuYsZai2sv-eoF0UuK9Srr-';
 
-  constructor(private receitaService: ReceitasService, private router: Router) { }
+  constructor(private router: Router, private receitasService: ReceitasService, private mensagensService: MensagensService) { }
 
   async ngOnInit() {
     // TODO: Eliminar mocks
-    this.receita = this.receitaService.getSelecionada();
-    console.log(this.receita);
-    const response = await this.receitaService.gerarQRCode(this.receita._id);
+    const receita = this.receitasService.getSelecionada();
+    if (receita) {
+      await this.atualizarDados(receita);
+    } else {
+      this.voltar();
+    }
+  }
+
+  // async ionViewWillEnter() {
+  //   const receita = this.receitasService.getSelecionada();
+  //   if (receita) {
+  //     await this.atualizarDados(receita);
+  //   } else {
+  //     this.voltar();
+  //   }
+  // }
+
+  private async atualizarDados(receita: Receita) {
+    this.receita = receita;
+    const response = await this.receitasService.gerarQRCode(this.receita._id);
     this.dataURL = response ? response : '';
   }
-    
-  public removerReceita() {
-    this.receitaService.remover(this.receita._id);
+
+  private voltar() {
+    this.mensagensService.erro('Dados inexistentes', 'Não foi possível obter os dados da receita selecionada!');
     this.router.navigate(['/lista-receitas']);
+  }
+
+  public async removerReceita() {
+    const success = await this.receitasService.remover(this.receita._id);
+    if (success) this.router.navigate(['/lista-receitas']);
   }
 
 }
