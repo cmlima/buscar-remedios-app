fa7ff6728fb7380af54ce8f011d1781bdc6bbbb0
Caio Moysés de Lima <caio.moyses.lima@gmail.com>
2021-05-29 18:42:10 -0300

    corrigindo parametros do mToKm pipe

--- src/app/services/custom-pipes/mToKm.pipe.ts
+++ src/app/services/custom-pipes/mToKm.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'mToKm' })
 export class MToKmPipe implements PipeTransform {
-  transform(n: number): string {
+  transform(n: number | string): string {
+    if (typeof n === 'string') n = parseFloat(n);
     return (n / 1000).toFixed(2);
   }
 }
