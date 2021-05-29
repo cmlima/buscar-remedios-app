a2728412983056f16c7a6ca3ad91881a89d87763
Caio Moysés de Lima <caio.moyses.lima@gmail.com>
2021-05-29 18:20:04 -0300

    so navega se retornou receita

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { ActionSheetController } from '@ionic/angular';
 import { ScannerService } from '../services/scanner.service';
 import { ReceitasService } from '../services/receitas.service';
+import { Receita } from '../services/entities';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -14,42 +15,10 @@ export class HomePage {
 
   public async lerQRCode() {
     const hash = await this.scannerService.scan();
-    if (hash) await this.receitasService.buscar(hash);
-    this.router.navigate(['lista-receitas']);
-    // TODO: Recuperar receita via API (ou mostrar mensagem de erro)
-    // + gravar no storage local + navegar para página lista-receitas
+    let receita: Receita | false = false;
+    if (hash) {
+      receita = await this.receitasService.buscar(hash);
+    }
+    if (receita) this.router.navigate(['lista-receitas']);
   }
-
-  // TODO: remover em produção
-  async presentActionSheet() {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Acessar Diretamente:',
-      buttons: [
-        {
-          text: 'Letícia (Lista de Receitas)',
-          role: 'navigation',
-          icon: 'open',
-          handler: () => this.router.navigate(['/lista-receitas'])
-        }, {
-          text: 'Caio (Detalhes da Receita)',
-          role: 'navigation',
-          icon: 'open',
-          handler: () => this.router.navigate(['/detalhes-receita'])
-        }, {
-          text: 'Pedro (Posto de Saúde - Mapa)',
-          role: 'navigation',
-          icon: 'open',
-          handler: () => this.router.navigate(['/mapa'])
-        }, {
-          text: 'Álef (Posto de Saúde - Detalhes)',
-          role: 'navigation',
-          icon: 'open',
-          handler: () => this.router.navigate(['/lista-postos'])
-        }
-      ]
-    });
-  
-    await actionSheet.present();
-  }
-
 }
